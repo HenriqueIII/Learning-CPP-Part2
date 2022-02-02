58fdca2142de4fcc1566f6b965aa40f14f03011b
HenriqueIII <paulogoncalves.m@gmail.com>
2022-02-02 21:26:37 +0000

    Resolvido Merge Conflict

--- Capitulo 2/Exemplos/Exemplo2.6.cpp
+++ Capitulo 2/Exemplos/Exemplo2.6.cpp
@@ -1,47 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
-
-// <<Definição de tipos>>
-
-typedef unsigned char byte;     // sizeof == 8 bits
-typedef unsigned short word;    // sizeof == 16 bits
-typedef unsigned int dword;    // sizeof == 32 bits
-
-// <<Definição de constantes>>
-
-static const size_t BITS_WORD = sizeof(word) * 8;
-
-// Base de numeração (65536)
-static const dword B_Radix = 0xFFFFL + 1;
-
-// Maior algarismo na base B_Radix == (B_Radix -1)
-
-static const word MAXDIGIT = 0xFFFF;
-
-class BigInt {
-// << Classes internas >>
-    class BigRep;       // Representação (body)
-    class BigRepPtr;    // Apontador inteligente para BigRep
-    class BigTmp;       // BigInt Temporário
-
-// << Atributo Único >>
-    BigRepPtr brp;      // Apontador inteligente para BigRep
-
-};
-
-class BigInt::BigRep {  // Body, representação de um BigInt
-    int count;      // BigInts que partilham este BigRep
-    size_t sz;      // Número de digitos
-    size_t dim;     // Dimensão do espaço alojado
-    int signal;     // -1 se negativo, 1 se positivo
-    word *v;        // A representação          
-};
-
-
-
-int main(int argc, char ** argv) {
-    
-=======
 // #include "BigInt.h"
 #include <cstring>
 
@@ -59,6 +16,5 @@ int main (int argc, char ** argv) {
     std::cout << str << std::endl;
     len -= saux - str;
     std::cout << len << std::endl; 
->>>>>>> 8fdf8d748d034c171e0c04c55ec7be678b1569d0
     return 0;
 }
