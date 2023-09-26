dcc7ee3b83f80296d511e4db33d041cc5555cb2c
Paulo Gonçalves <paulogoncalves.m@gmail.com>
2023-09-26 17:36:37 +0100

    Delete Capitulo 3/exemplos/build directory

--- Capitulo 3/exemplos/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/paulo/OneDrive/Documentos/GitHub/Learning-CPP-Part2/Capitulo 3/exemplos
-# Build directory: C:/Users/paulo/OneDrive/Documentos/GitHub/Learning-CPP-Part2/Capitulo 3/exemplos/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
